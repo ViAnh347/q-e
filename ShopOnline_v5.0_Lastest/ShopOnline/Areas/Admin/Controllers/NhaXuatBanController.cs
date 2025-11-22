bcbbd63ca219bcc45f5dc8bda1487be89ee0cad7
ViAnh347 <nguyenvanvietanh1209@gmail.com>
2025-11-22 20:01:59 +0700

    First commit

---
+++ ShopOnline_v5.0_Lastest/ShopOnline/Areas/Admin/Controllers/NhaXuatBanController.cs
@@ -0,0 +1,104 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ShopOnline.Models;
+
+namespace ShopOnline.Areas.Admin.Controllers
+{
+	public class NhaXuatBanController : Controller
+	{
+		private ShopOnlineEntities db = new ShopOnlineEntities();
+		
+		// GET: NhaXuatBan
+		public ActionResult Index()
+		{
+			return View(db.NhaXuatBan.ToList());
+		}
+		
+		// GET: NhaXuatBan/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
+		
+		// POST: NhaXuatBan/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create([Bind(Include = "ID,TenNhaXuatBan")] NhaXuatBan nhaXuatBan)
+		{
+			if (ModelState.IsValid)
+			{
+				db.NhaXuatBan.Add(nhaXuatBan);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			
+			return View(nhaXuatBan);
+		}
+		
+		// GET: NhaXuatBan/Edit/5
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			NhaXuatBan nhaXuatBan = db.NhaXuatBan.Find(id);
+			if (nhaXuatBan == null)
+			{
+				return HttpNotFound();
+			}
+			return View(nhaXuatBan);
+		}
+		
+		// POST: NhaXuatBan/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "ID,TenNhaXuatBan")] NhaXuatBan nhaXuatBan)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(nhaXuatBan).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(nhaXuatBan);
+		}
+		
+		// GET: NhaXuatBan/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			NhaXuatBan nhaXuatBan = db.NhaXuatBan.Find(id);
+			if (nhaXuatBan == null)
+			{
+				return HttpNotFound();
+			}
+			return View(nhaXuatBan);
+		}
+		
+		// POST: NhaXuatBan/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			NhaXuatBan nhaXuatBan = db.NhaXuatBan.Find(id);
+			db.NhaXuatBan.Remove(nhaXuatBan);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
+		
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
+}
---
+++ ShopOnline_v5.0_Lastest/ShopOnline/Models/GioHangModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopOnline.Models
+{
+    public class SanPhamTrongGio
+    {
+        public Sach sach { get; set; }
+        public int soLuongTrongGio { get; set; }
+    }
+}
